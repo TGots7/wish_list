71dd05b1121b54184d37a3fa06c709bea522f99e
TGots7 <tylergotto17@gmail.com>
2017-09-04 17:36:01 -0400

    sign up & log in

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -15,8 +15,35 @@ class ApplicationController < Sinatra::Base
 		erb :'/users/sign_up'
 	end
 
+	post '/sign_up' do 
+		@user = User.new(:name => params[:name])
+		@user.email = params[:email]
+		@user.password = params[:password]
+		@user.save
+
+		redirect '/log_in'
+	end
+
 	get '/log_in' do 
 		erb :'/users/log_in'
+	end
+
+	post '/log_in' do 
+		@user = User.find_by(email: params[:email])
+
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+			redirect '/users/home'
+		else
+			redirect '/log_in'
+		end
+	end
+
+	get '/users/home' do 
+		@user = User.find(session[:user_id])
+		
+
+		erb :'/users/home'
 	end
 
 end
